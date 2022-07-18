c2e4d818b4a1b6c88cbff5e5de07312ee2142c5e
LeoKim <leo3179@naver.com>
2022-07-18 14:25:05 +0900

    ADD / @nestjs-swagger 설치
    - 스웨거 설치 및 설정 완료

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { BaseAPIDocumentation } from './config/baseApiDocs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
+
+  const documentOptions = new BaseAPIDocumentation().initializeOptions();
+  const document = SwaggerModule.createDocument(app, documentOptions);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
